2baf2b50da062b8ef2a193579299de26e97443f1
braydend <braydendiluca@gmail.com>
2022-04-07 10:42:04 +1000

    Add git aliases shell file

---
+++ git/git-aliases.sh
@@ -0,0 +1,17 @@
+## Git aliases
+
+alias gst="git status"
+
+alias gcmsg="git commit --message"
+
+alias gcb="git checkout -b"
+
+alias gco="git checkout"
+
+alias gapa="git add --patch"
+
+alias gcp="git cherry-pick"
+
+alias gfix="git commit --fixup"
+
+alias gash="git rebase --autosquash --interactive"
